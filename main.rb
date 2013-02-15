62be68144f7304474e8791ed4beca68ff82ff9bb
Alex Angelini <alex.louis.angelini@gmail.com>
2013-02-15 14:43:43 -0500

    Cleanup

--- main.rb
+++ main.rb
@@ -6,36 +6,31 @@ require 'bcrypt'
 SECRET = 'S3KR3T-K3Y'
 
 set :public_folder, '../coupon-client'
+set :views, '../coupon-client/views'
 
 
 get '/' do
   content_type 'text/html'
-  erb :index
+  File.read(File.join('..', 'coupon-client', 'index.html'))
 end
 
-get '/register' do
-  content_type 'text/html'
-  erb :register
-end
-
-get '/profile' do
-  content_type 'text/html'
-  erb :profile
-end
-
-def authenticate?
+def authenticate?(status)
   if request.cookies['key'].nil? || request.cookies['user_key'].nil?
-    status 403
-    response.write({ :error => '"user_key" and "key" Cookie Required' }.to_json)
-    response.close()
+    if status == true
+      status 403
+      response.write({ :error => '"user_key" and "key" Cookie Required' }.to_json)
+      response.close()
+    end
     return false
   end
 
   auth = BCrypt::Password.new(request.cookies['key'])
   if auth != (request.cookies['user_key'] + SECRET)
-    status 403
-    response.write({ :error => 'Invalid Key' }.to_json)
-    response.close()
+    if status == true
+      status 403
+      response.write({ :error => 'Invalid Key' }.to_json)
+      response.close()
+    end
     return false
   end
 
@@ -67,19 +62,17 @@ post '/api/login' do
     status 400
     return { :error => 'Password Required' }.to_json
   end
-  
+
   res = @conn.exec('SELECT id
-                   FROM users
-                   WHERE (username = $1
-				   AND suspended = $2) OR
-				  (email = $3
-				   AND suspended = $2)',[@data['username'], true, @data['email']])
-				   
- if res.num_tuples != 0
-	status 403
-	return { :error => 'User suspended'}.to_json
+                    FROM users
+                    WHERE (username = $1 AND suspended = $2)
+                    OR (email = $3 AND suspended = $2)',[@data['username'], true, @data['email']])
+
+  if res.num_tuples != 0
+    status 403
+    return { :error => 'User suspended'}.to_json
   end
-				   
+
   res = @conn.exec('SELECT id, username, password, suspended
                    FROM users
                    WHERE username = $1
@@ -91,19 +84,28 @@ post '/api/login' do
   end
 
   password = BCrypt::Password.new(res[0]['password'])
-  
+
   if password != @data['password']
     status 403
     return { :error => 'Password Does Not Match' }.to_json
   end
 
-  
   user_key = res[0]['id'] + ':' + res[0]['username']
   key = BCrypt::Password.create(user_key + SECRET)
   { :id => res[0]['id'],
     :username => res[0]['username'],
     :user_key => user_key,
     :key => key }.to_json
+end
+
+get '/api/login/test' do
+  if authenticate?(false)
+    { :status => true,
+      :user_id => @user_id,
+      :username => @username }.to_json
+  else
+    { :status => false }.to_json
+  end
 end
 
 post '/api/users' do
@@ -126,13 +128,13 @@ post '/api/users' do
     status 400
     return { :error => 'Username/Email Already Taken' }.to_json
   end
-  
+
   hash = BCrypt::Password.create(@data['password'])
 
   @conn.exec('INSERT INTO users (username, email, password, accounttype) VALUES ($1, $2, $3, $4)',
               [@data['username'], @data['email'], hash, @data['accounttype']])
 
-  
+
 #  @conn.exec('INSERT INTO users (username, email, password, firstname, lastname, address, phonenumber, suspended, accounttype, paypalaccountname, creditcardnumber, creditcardexpirydate)
 #              VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)',
 #              [@data['username'], @data['email'], hash, @data['firstname'], @data['lastname'], @data['address'], @data['phonenumber'], false, @data['accounttype'], @data['paypalaccountname'], @data['creditcardnumber'], @data['creditcardexpirydate']])
@@ -142,8 +144,8 @@ post '/api/users' do
 end
 
 get '/api/users/:id' do |id|
-  return if authenticate? == false
-# * => username, email, password, firstname, lastname, address, phonenumber, suspended, accounttype, paypalaccountname, creditcardnumber, creditcardexpirydate
+  return if authenticate?(true) == false
+
   res = @conn.exec('SELECT * FROM users
                    WHERE id = $1', [id])
 
@@ -155,29 +157,29 @@ get '/api/users/:id' do |id|
   res[0].to_json
 end
 
-post '/api/coupons' do 
-  return if authenticate? == false
-  
-    if @data['name'].nil? || @data['description'].nil? || @data['logo_url'].nil?
+post '/api/coupons' do
+  return if authenticate?(true) == false
+
+  if @data['name'].nil? || @data['description'].nil? || @data['logo_url'].nil?
     status 400
     return { :error => 'Incomplete POST Data' }.to_json
   end
-	
-	if @data['name'].length < 3
+
+  if @data['name'].length < 3
     status 400
     return { :error => 'Coupon name too short < 3!' }.to_json
   end
-  
+
   if @data['logo_url'].length < 3
     status 400
     return { :error => 'Logo url too short < 3!' }.to_json
   end
-  
-   if @data['description'].length < 0
+
+  if @data['description'].length < 0
     status 400
     return { :error => 'Description must be included!' }.to_json
   end
-  
+
   if @data['description'].length >= 1000
     status 400
     return { :error => 'Description Length > 1000' }.to_json
@@ -192,7 +194,7 @@ post '/api/coupons' do
     status 400
     return { :error => 'Coupon Already Exists' }.to_json
   end
-  
+
   @conn.exec('INSERT INTO coupons (name, description, logo_url, owner_id, creator_id, amount, price)
               VALUES ($1, $2, $3, $4, $5, $6, $7)',
               [@data['name'], @data['description'], @data['logo_url'], @user_id, @user_id, 1, @data['price']])
@@ -205,8 +207,8 @@ post '/api/coupons' do
 end
 
 get '/api/coupons' do
-  return if authenticate? == false
-  
+  return if authenticate?(true) == false
+
   res = @conn.exec('SELECT id, name, description, logo_url, owner_id, amount, price, coupontype, expirydate, useramountlimit
                    FROM coupons')
 
@@ -219,7 +221,7 @@ get '/api/coupons' do
 end
 
 get '/api/coupons/:id' do |id|
-  return if authenticate? == false
+  return if authenticate?(true) == false
 
   res = @conn.exec('SELECT id, name, description, logo_url, owner_id, amount, price, coupontype, expirydate, useramountlimit
                    FROM coupons
@@ -233,77 +235,62 @@ get '/api/coupons/:id' do |id|
   res[0].to_json
 end
 
-post '/api/ui/register' do
-	#return if authenticate? == true
-	
-	content_type 'application/json'
-	
-	status 200
-	return {
-		:status => 'OK',
-		:tmpl => {
-			:nav => (erb :register_nav, :layout => :nulllayout),
-			:content => (erb :register_content, :layout => :nulllayout)
-		}
-	}.to_json
+get '/api/ui/register' do
+  return {
+    :status => 'OK',
+    :tmpl => {
+      :nav => (erb :register_nav, :layout => :nulllayout),
+      :content => (erb :register_content, :layout => :nulllayout)
+    }
+  }.to_json
 end
 
-post '/api/ui/index' do
-	#return if authenticate? == true
-	
-	content_type 'application/json'
-	
-	status 200
-	return {
-		:status => 'OK',
-		:tmpl => {
-			:nav => @user_id ? (erb :profile_nav, :layout => :nulllayout) : (erb :index_nav, :layout => :nulllayout),
-			:content => (erb :index_content, :layout => :nulllayout)
-		}
-	}.to_json
+get '/api/ui/index' do
+  return {
+    :status => 'OK',
+    :tmpl => {
+      :nav => (erb :index_nav, :layout => :nulllayout),
+      :content => (erb :index_content, :layout => :nulllayout)
+    }
+  }.to_json
 end
 
-post '/api/ui/profile' do
-	#return if authenticate? == true
-	
-	content_type 'application/json'
-	
-	status 200
-	return {
-		:status => 'OK',
-		:tmpl => {
-			:nav => (erb :profile_nav, :layout => :nulllayout),
-			:content => (erb :profile_content, :layout => :nulllayout)
-		}
-	}.to_json
+get '/api/ui/guest' do
+  return {
+    :status => 'OK',
+    :tmpl => {
+      :nav => (erb :guest_nav, :layout => :nulllayout),
+      :content => (erb :guest_content, :layout => :nulllayout)
+    }
+  }.to_json
 end
 
-post '/api/ui/editprofile' do
-	#return if authenticate? == true
-	
-	content_type 'application/json'
-	
-	status 200
-	return {
-		:status => 'OK',
-		:tmpl => {
-			:nav => (erb :profile_nav, :layout => :nulllayout),
-			:content => (erb :editprofile_content, :layout => :nulllayout)
-		}
-	}.to_json
+get '/api/ui/profile' do
+  return {
+    :status => 'OK',
+    :tmpl => {
+      :nav => (erb :profile_nav, :layout => :nulllayout),
+      :content => (erb :profile_content, :layout => :nulllayout)
+    }
+  }.to_json
 end
 
-post '/api/ui/createcoupon' do
-	#return if authenticate? == true
-	
-	content_type 'application/json'
-	
-	status 200
-	return {
-		:status => 'OK',
-		:tmpl => {
-			:nav => (erb :createcoupon_nav, :layout => :nulllayout),
-			:content => (erb :createcoupon_content, :layout => :nulllayout)
-		}
-	}.to_json
+get '/api/ui/editprofile' do
+  return {
+    :status => 'OK',
+    :tmpl => {
+      :nav => (erb :editprofile_nav, :layout => :nulllayout),
+      :content => (erb :editprofile_content, :layout => :nulllayout)
+    }
+  }.to_json
+end
+
+get '/api/ui/createcoupon' do
+  return {
+    :status => 'OK',
+    :tmpl => {
+      :nav => (erb :createcoupon_nav, :layout => :nulllayout),
+      :content => (erb :createcoupon_content, :layout => :nulllayout)
+    }
+  }.to_json
 end
--- tables.sql
+++ tables.sql
@@ -5,27 +5,27 @@ CREATE TABLE users (
     password varchar(60),
     firstname varchar(200),
     lastname varchar(200),
-	address varchar(200),
-	phonenumber varchar(200),
-	suspended boolean,
-	accounttype varchar(200),
-	paypalaccountname varchar(200),
-	creditcardnumber varchar(200),
-	creditcardexpirydate date	
+    address varchar(200),
+    phonenumber varchar(200),
+    suspended boolean,
+    accounttype varchar(200),
+    paypalaccountname varchar(200),
+    creditcardnumber varchar(200),
+    creditcardexpirydate date
 );
 
 CREATE TABLE coupons (
     id serial PRIMARY KEY,
     name varchar(100),
-	coupontype varchar(300),
+    coupontype varchar(300),
     description varchar(1000),
     logo_url varchar(200),
-	owner_id serial,
-	creator_id serial,
-	amount integer,
-	price float,
-	expirydate date,
-	useramountlimit integer,
-	FOREIGN KEY (owner_id) REFERENCES users(id),
-	FOREIGN KEY (creator_id) REFERENCES users(id)
+    owner_id serial,
+    creator_id serial,
+    amount integer,
+    price float,
+    expirydate date,
+    useramountlimit integer,
+    FOREIGN KEY (owner_id) REFERENCES users(id),
+    FOREIGN KEY (creator_id) REFERENCES users(id)
 );
